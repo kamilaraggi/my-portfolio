407b9c1820f3944a04a60c6b888106ea810fd8eb
kamilaraggi <kamilaraggi@hotmail.com>
2022-01-07 20:21:57 -0500

    deploying

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ function Foo() {
         className="blue"
        
         copyrights=""
-        links={<ul><li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/kamila-raggi-676039101/">LinkedIn</a></li><li><a className="grey-text text-lighten-3" href="https://github.com/kamilaraggi">GitHub</a></li><li><a className="grey-text text-lighten-3" href={pdf}>Resume</a></li></ul>}
+        links={<ul><li><a className="grey-text text-lighten-3" href="https://www.linkedin.com/in/kamila-raggi-676039101/">LinkedIn</a></li><li><a className="grey-text text-lighten-3" href="https://github.com/kamilaraggi">GitHub</a></li><li><a className="grey-text text-lighten-3" href={pdf}>Resume</a></li><li><a className="grey-text text-lighten-3" href="mailto:kamilaraggi@hotmail.com">E-mail Me</a></li></ul>}
         moreLinks={<a className="grey-text text-lighten-4 right" href="#!"></a>}
       >
         <h5 className="white-text">
